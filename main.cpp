f2f937850276eaea1a1a18108b669beecd871ad8
maxutka99 <linuxtop@yandex.ru>
2021-04-25 22:46:52 +0300

    main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <array>
+#include <limits.h>
+
+#include <sys/typs.h>
+#include <sys/sysinfo.h>
+#include <X11/Xlib.h>
+
